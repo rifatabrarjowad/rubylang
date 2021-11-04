d56469eac85cb691053b2e8aefe0d7bca97fc3b1
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-11-05 02:32:19 +0600

    year check

--- class/day04/01.rb
+++ class/day04/01.rb
@@ -1,3 +1,18 @@
-a = 5
-
-puts a
+#leap year funder
+flag = false
+puts "Please enter a year"
+year = gets.chomp.to_i      #get year
+if year % 4 == 0
+  if year % 100 == 0
+    if year % 400 == 0
+      flag = true
+    end
+  else
+    flag = true
+  end
+end
+if flag
+  puts "This is a leap year"
+else
+  puts "This is not a leap year"
+end
