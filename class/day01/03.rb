da897ea42fda13f5a9944fd8d2efd32f07941803
rifatabrarjowad <rajdiprifat2004@gmail.com>
2021-11-08 23:51:59 +0600

    update

--- class/day01/03.rb
+++ class/day01/03.rb
@@ -1,1 +1,18 @@
-puts "Hello World"
+#leap year funder
+flag = false
+puts "Please enter a year"
+year = gets.chomp.to_i      #get year
+if year % 4 == 0
+  if year % 100 == 0
+    if year % 400 == 0
+      flag = true
+    end
+  else
+    flag = true
+  end
+end
+if flag
+  puts "This is a leap year"
+else
+  puts "This is not a leap year"
+end
